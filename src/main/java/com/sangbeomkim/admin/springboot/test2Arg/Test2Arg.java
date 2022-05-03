ea24455d2092ad08a1c9d0cce6570c977db558ab
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-05-03 23:41:29 +0900

    simple server initialization

---
+++ src/main/java/com/sangbeomkim/admin/springboot/test2Arg/Test2Arg.java
@@ -0,0 +1,12 @@
+package com.sangbeomkim.admin.springboot.test2Arg;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test2Arg{
+    private String string1;
+    private String string2;
+
+}
---
+++ src/main/java/com/sangbeomkim/admin/springboot/web/HelloController.java
@@ -0,0 +1,42 @@
+package com.sangbeomkim.admin.springboot.web;
+
+import com.sangbeomkim.admin.springboot.test2Arg.Test2Arg;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.tags.Param;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/get_test")
+    public Test2Arg getTest(
+            @RequestParam(value = "var1") String var1,
+            @RequestParam(value = "var2") String var2) {
+
+        Test2Arg test2Arg = new Test2Arg();
+        test2Arg.setString1(var2);
+        test2Arg.setString2(var1);
+
+        return test2Arg;
+    }
+//    @Configuration
+//    @EnableWebSecurity
+//    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity httpSecurity) throws Exception{
+//            httpSecurity.httpBasic().disable();
+//        }
+//
+//    }
+
+}
